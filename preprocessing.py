f89c819464ced519336668bde974c8036279192f
likhithathalakanti <43198406+likhithathalakanti@users.noreply.github.com>
2024-07-05 15:57:25 -0700

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,5 @@
+# Replace xxxx with downloaded .csv file from kaggle set https://www.kaggle.com/datasets/sobhanmoosavi/us-accidents
+#My data set was from Febuary 2016 to December 2021
+import pandas as pd
+df = pd.read_csv('xxxx.csv',header=None)
+df
