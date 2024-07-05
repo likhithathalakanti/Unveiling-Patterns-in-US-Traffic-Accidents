9bf9944938a37580ab74105ac262659359dfaa72
likhithathalakanti <43198406+likhithathalakanti@users.noreply.github.com>
2024-07-05 16:31:46 -0700

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,4 +1,6 @@
 # Train model
+from pyspark.ml.regression import LinearRegression
+
 def train_model(df):
     lr = LinearRegression(featuresCol="features", labelCol="Severity")
     model = lr.fit(df)
